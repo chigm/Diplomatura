095bcc0d76cb08a1cfe908fec11966e1dade12e3
Virginia GarciaMansilla <vagm.chi@gmail.com>
2023-10-09 11:59:06 -0300

    Modulo2 JS

---
+++ Clase 13 JS/ejemplo01.js
@@ -0,0 +1,17 @@
+/*declarando variables*/
+
+/*var nombre='Virginia'
+var nombre='Pepe'*/
+
+/*let nombre='Virginia'*/
+
+const nombre='Virginia';
+const edad= 27;
+
+
+/*como lo vemos*/
+
+document.write(nombre + " asi que tienes " + edad)
+
+console.log(nombre + edad)
+console.log(`Mi nombre es ${nombre} y tengo ${edad} años.`)
---
+++ Clase 13 JS/ejemplo03.js
@@ -0,0 +1,21 @@
+const frutas =  ['Manzana', 'Banana', 'Pera', 'Palta'];
+
+console.log(frutas)
+//document.write(frutas)
+console.log(frutas.length); //4
+
+console.log(frutas[1]); //banana
+
+var ultimo = frutas [frutas.length-1]; //palta//siempre va imprimir el ultimo
+
+
+console.log(ultimo)
+
+/*const seleccionado = 2;*/
+let seleccionado=2
+
+console.log(frutas [ seleccionado ]); //pera
+
+seleccionado=1
+
+console.log(frutas [seleccionado]);//banana
